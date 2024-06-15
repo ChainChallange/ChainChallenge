a2ee15e1b35bb5e1733b549247531e7b1af90578
YagoDevs <110265917+YagoDevs@users.noreply.github.com>
2024-06-15 00:09:17 -0300

    Atualizações pagina doc

--- frontend/app/documentation/page.tsx
+++ frontend/app/documentation/page.tsx
@@ -26,9 +26,9 @@ export default function Documentation() {
 
   return (
     <main className="flex min-h-screen w-full gap-14 bg-[#121418] pt-44 px-72">
-      <div className='flex flex-col w-1/6 h-screen text-[20px]'>
-        <p className="text-[#9D9D9D] mb-6">API Reference</p>
-        <div className="mt-4">
+      <div className='flex flex-col w-1/6 h-screen text-[20px] gap-24'>
+        <p className="text-[#9D9D9D]">API Reference</p>
+        <div className="flex flex-col gap-4">
           <div>
             <div onClick={() => toggle('components')} className="cursor-pointer flex justify-between items-center text-white">
               Components
@@ -42,19 +42,7 @@ export default function Documentation() {
               </div>
             )}
           </div>
-          <div className="mt-4">
-            <div onClick={() => toggle('hooks')} className="cursor-pointer flex justify-between items-center text-white">
-              Hooks
-              {open.hooks ? <FaChevronDown /> : <FaChevronRight />}
-            </div>
-            {open.hooks && (
-              <div className="ml-5 mt-2 text-[#9D9D9D]">
-                {/* Hooks content here */}
-                <div>useHookExample</div>
-              </div>
-            )}
-          </div>
-          <div className="mt-4">
+          <div className="">
             <div onClick={() => toggle('routes')} className="cursor-pointer flex justify-between items-center text-white">
               Routes
               {open.routes ? <FaChevronDown /> : <FaChevronRight />}
@@ -73,7 +61,7 @@ export default function Documentation() {
         <div className="flex text-[20px] gap-4">
           <div className="">References</div>
         </div>
-        <h1 className="text-3xl font-semibold">References</h1>
+        <h1 className="text-3xl font-semibold mt-20">References</h1>
       </div>
       <div className="text-[20px]">
         <p>On This Page</p>
