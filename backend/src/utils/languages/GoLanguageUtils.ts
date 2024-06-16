fe82426ab56471823419963a98af5e9349762835
root <root@Notebook-0162>
2024-06-16 02:48:38 -0300

    feat: go challanges working

--- backend/src/utils/languages/GoLanguageUtils.ts
+++ backend/src/utils/languages/GoLanguageUtils.ts
@@ -1,9 +1,15 @@
 import { executionUtils } from '../ExecutionUtils';
+import { fileUtils } from '../FileUtilts';
 import { ILanguageFileNames, ILanguageUtils } from './ILanguageUtils';
 
 class GoLanguageUtils implements ILanguageUtils {
     runTestCommandShell(filename: string, outputFile: string) {
-        return executionUtils.runShellAndConcatOutputFile(`go test -failfast=false ${filename}`, outputFile);
+        fileUtils.writeFileSync(`${filename}go.mod`, `module chainchallenge
+        go 1.23`)
+
+        executionUtils.runShellSync(`cd ${filename} && go test -failfast=false`);
+
+        return fileUtils.readFileSync(outputFile);
     }
 
     getFileNames(): ILanguageFileNames{
