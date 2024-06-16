4b628a68dce58d70de3dcbbf6a981cc133e7869c
root <root@Notebook-0162>
2024-06-16 15:17:08 -0300

    feat: ranking captching changes

--- backend/src/services/RankingService.ts
+++ backend/src/services/RankingService.ts
@@ -1,4 +1,5 @@
-import { IParticipantRankingCreate } from "../models/IParticipantRanking";
+import { IApplicant } from "../models/IApplicant";
+import { IParticipantRanking, IParticipantRankingCreate } from "../models/IParticipantRanking";
 import { IWallet } from "../models/types/IWallet";
 import { applicantRepository } from "../repositories/ApplicantRepository";
 import { rankingRepository } from "../repositories/RankingRepository";
@@ -14,19 +15,34 @@ class RankingService {
 
     generate() {
         const applicants: IParticipantRankingCreate[] = applicantRepository.list().map(applicant => {
-            return {    
-                applications_accepted_quantity: applicant.applications_accepted_quantity,
-                attempts_quantity: applicant.attempts_quantity,
-                challenges_quantity: Object.values(applicant.challenges).length,
-                score: applicant.total_score,
-                wallet: applicant.wallet,
-                image_link: applicant.image_link,
-                nickname: applicant.nickname   
-            }
+            return this.applicantToParticipantRanking(applicant, 0);
         })
 
         return rankingRepository.createAndOrder(applicants);
     }
+
+    updateByApplicant(applicant: IApplicant) {
+        const ranking = this.findByWallet(applicant.wallet);
+        if(!ranking) {
+            return null;
+        }
+
+        rankingRepository.update(applicant.wallet, this.applicantToParticipantRanking(applicant, ranking.position))
+    }
+
+    private applicantToParticipantRanking(applicant: IApplicant, position: number): IParticipantRanking {
+        return {
+            applications_accepted_quantity: applicant.applications_accepted_quantity,
+            attempts_quantity: applicant.attempts_quantity,
+            challenges_quantity: Object.values(applicant.challenges).length,
+            score: applicant.total_score,
+            wallet: applicant.wallet,
+            image_link: applicant.image_link,
+            nickname: applicant.nickname,
+            position
+        }
+    }
+
 }
 
 export const rankingService = new RankingService();
